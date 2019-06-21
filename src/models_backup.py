531e3bdf4549e0bb571b8d98094e28c324ebf4fd
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-06-21 13:24:04 +0900

    Misclassifications

--- src/models_backup.py
+++ src/models_backup.py
@@ -114,32 +114,6 @@ class PatchWiseModel(BaseModel):
                         loss.item(),
                         100 * correct / total
                     ))
-                
-                if 100 * correct / total > 75:
-
-                    self.network.eval()
-
-                    test_loader = DataLoader(
-                        dataset=PatchWiseDataset(path=self.args.dataset_path + VALIDATION_PATH, stride=self.args.patch_stride),
-                        batch_size=1,
-                        shuffle=False,
-                        num_workers=4
-                    )
-        
-                    print('\nEvaluating....')
-
-                    for images, labels in test_loader:
-
-                        if self.args.cuda:
-                            images = images.cuda()
-
-                        with torch.no_grad():
-                            output = self.network(Variable(images))
-
-                         _, predicted = torch.max(output.data, 1)
-                         predicted = LABELS[predicted.cpu().numpy()]
-                         print("p ",predicted,"l ",label)
-                         exit(0)
 
             train_loss /= len(self.train_loader.dataset)
             train_acc = 100 * correct / total
