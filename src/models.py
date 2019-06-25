290c725e063cbe66ff1dab2108c550678a9967a6
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-06-25 14:20:57 +0900

    Update models.py

--- src/models.py
+++ src/models.py
@@ -38,7 +38,7 @@ class BaseModel:
                 print('Loading "patch-wise" model...')
                 self.network.load_state_dict(torch.load(self.weights))
         except:
-            print('Failed to load pre-trained network '+self.network.name())
+            print('Failed to load pre-trained network '+self.network.name()+' from '+self.weights)
 
     def save(self):
         print('Saving model to "{}"'.format(self.weights))
