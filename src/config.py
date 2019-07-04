ebc4ea2445653006ac44609a57dd8dee789461ed
Ninad Akolekar <ninad.akolekar@gmail.com>
2019-07-04 17:30:15 +0900

    Add doc for config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Defines configuration parameters for the experiment
+"""
 LABELS = ['grape', 'round', 'stellate']
+"""list: The names of classes/labels associated with the images"""
+
 IMAGE_SIZE = (2048, 1536)
+"""tuple: A tuple of two integers denoting the height and width of the images respectively"""
+
 PATCH_SIZE = 512
+"""int: The size of patches extracted from the image"""
